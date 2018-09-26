02b310da45613e4550417a1e2a9ff31ee8253a28
Venkata Krishna Nelapati <krishna_nelapati_venkata@network.lilly.com>
2018-09-26 09:54:46 -0400

    intial commit

---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <h1>
+        {{part.title}}
+    </h1>
+    <div>
+        {{part.description}}
+    </div>
+</div>
+</template>
+
+<script>
+import getPartsMixin from './get-parts-mixin';
+
+export default {
+  name: 'PartInfo',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
+  mixins: [getPartsMixin],
+  computed: {
+    part() {
+      const { partType, id } = this;
+      return this.parts[partType].find(part => part.id === +id);
+    },
+  },
+};
+</script>
